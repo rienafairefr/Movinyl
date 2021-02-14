340573d6db986798700f03d6a5ead36ffd919986
Matthieu Berthomé <matthieu@mmea.fr>
2021-02-14 16:07:33 +0100

    no need to build examples

--- scripts/prepare_for_build_Linux.sh
+++ scripts/prepare_for_build_Linux.sh
@@ -23,11 +23,8 @@ cd ~/opencv_build/opencv && mkdir build && cd build
 
 cmake3 -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_C_EXAMPLES=ON \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
     -D OPENCV_GENERATE_PKGCONFIG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
-    -D BUILD_EXAMPLES=ON ..
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules
 
 make
 
