a216462a04a632ee24b23746609d65d6e0239a04
Matthieu Berthomé <matthieu@mmea.fr>
2021-02-14 14:38:37 +0100

    build whl in ci

---
+++ scripts/prepare_for_build_Linux.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+yum update
+
+yum -y install epel-release git gcc gcc-c++ cmake3 qt5-qtbase-devel \
+    python python-devel python-pip cmake python-devel python34-numpy \
+    gtk2-devel libpng-devel jasper-devel openexr-devel libwebp-devel \
+    libjpeg-turbo-devel libtiff-devel libdc1394-devel tbb-devel numpy \
+    eigen3-devel gstreamer-plugins-base-devel freeglut-devel mesa-libGL \
+    mesa-libGL-devel boost boost-thread boost-devel libv4l-devel
+
+mkdir ~/opencv_build && cd ~/opencv_build
+git clone https://github.com/opencv/opencv.git --depth 1 --branch 4.4.0
+git clone https://github.com/opencv/opencv_contrib.git --depth 1 --branch 4.4.0
+cd ~/opencv_build/opencv && mkdir build && cd build
+
+cmake3 -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_C_EXAMPLES=ON \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_GENERATE_PKGCONFIG=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules \
+    -D BUILD_EXAMPLES=ON ..
+
+make
+
+sudo make install
+
+sudo ln -s /usr/local/lib64/pkgconfig/opencv4.pc /usr/share/pkgconfig/
+
+sudo ldconfig
+
+pkg-config --modversion opencv4
