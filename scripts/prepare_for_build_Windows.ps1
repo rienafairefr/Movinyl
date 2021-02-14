7621a8c671079d2ad8b956589c915ff81572e6fe
Matthieu Berthomé <matthieu@mmea.fr>
2021-02-14 18:45:30 +0100

    debug

--- scripts/prepare_for_build_Windows.ps1
+++ scripts/prepare_for_build_Windows.ps1
@@ -8,6 +8,8 @@ mkdir ~/opencv_build && cd ~/opencv_build
 git clone https://github.com/opencv/opencv.git --depth 1 --branch  4.4.0
 git clone https://github.com/opencv/opencv_contrib.git --depth 1 --branch  4.4.0
 cd ~/opencv_build/opencv && mkdir build && cd build
+ls -al
+cat *
 
 cmake -D CMAKE_BUILD_TYPE=RELEASE `
     -D OPENCV_GENERATE_PKGCONFIG=ON `
