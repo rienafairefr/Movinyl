f5bab6a316fb73b43073368d6da5a86ef55c7c71
Matthieu Berthomé <matthieu@mmea.fr>
2021-02-14 16:35:28 +0100

    numpy dev-req

--- scripts/prepare_for_build_Linux.sh
+++ scripts/prepare_for_build_Linux.sh
@@ -24,7 +24,7 @@ cd ~/opencv_build/opencv && mkdir build && cd build
 cmake3 -D CMAKE_BUILD_TYPE=RELEASE \
     -D CMAKE_INSTALL_PREFIX=/usr/local \
     -D OPENCV_GENERATE_PKGCONFIG=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules
+    -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 
 make
 
